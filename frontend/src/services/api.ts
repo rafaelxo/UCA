1db78c28b4078bce57312f5c8e0874fa255f3a03
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2026-01-17 02:58:59 -0300

    API CRUD base

---
+++ frontend/src/services/api.ts
@@ -0,0 +1,45 @@
+import axios from "axios";
+import type { Property } from "../types";
+
+const API_URL = "http://localhost:3000/api";
+
+export const api = axios.create({
+  baseURL: API_URL,
+});
+
+export const authService = {
+  login: (email: string, password: string) => {
+    return api.post("/auth/login", { email, password });
+  },
+
+  register: (
+    email: string,
+    password: string,
+    name: string,
+    cellphone: string,
+  ) => {
+    return api.post("/auth/register", { email, password, name, cellphone });
+  },
+};
+
+export const propertyService = {
+  getAll: () => {
+    return api.get("/properties");
+  },
+
+  getById: (id: number) => {
+    return api.get(`/properties/${id}`);
+  },
+
+  create: (property: Property) => {
+    return api.post("/properties", property);
+  },
+
+  update: (id: number, property: Property) => {
+    return api.put(`/properties/${id}`, property);
+  },
+
+  delete: (id: number) => {
+    return api.delete(`/properties/${id}`);
+  },
+};
