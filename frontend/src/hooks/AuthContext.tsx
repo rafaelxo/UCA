90ada52d9ca3a499b544f7b8dd8b172f2a9d3b00
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2026-01-17 03:12:00 -0300

    Auth Context

---
+++ frontend/src/hooks/AuthContext.tsx
@@ -0,0 +1,22 @@
+import { createContext } from 'react';
+import type { ReactNode } from 'react';
+import { useAuth } from "./useAuth";
+import type { User } from "../types";
+
+type AuthContextType = {
+  user: User | null;
+  token: string | null;
+  loading: boolean;
+  login: (email: string, password: string) => Promise<void>;
+  logout: () => void;
+};
+
+export const AuthContext = createContext<AuthContextType>(
+  {} as AuthContextType,
+);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const auth = useAuth();
+
+  return <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>;
+};
