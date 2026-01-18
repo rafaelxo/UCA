591683328eab59fcdaf1afa43011e658c9e790a7
Rafael Xavier Oliveira <rafaelxo2007@gmail.com>
2026-01-18 19:29:36 -0300

    Property list integration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
+import PropertyList from "./pages/PropertyList";
 
 function App() {
   return (
@@ -17,6 +18,14 @@ function App() {
             element={
               <ProtectedRoute>
                 <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/properties"
+            element={
+              <ProtectedRoute>
+                <PropertyList />
               </ProtectedRoute>
             }
           />
