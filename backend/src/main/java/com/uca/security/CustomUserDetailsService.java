4f56d3534500ee1cdab4b44fdd855990ccae03e8
rafaelxo <rafaelxo2007@gmail.com>
2026-01-27 01:56:10 -0300

    User details service
    Co-Authored-By: faelxg <254059177+faelxg@users.noreply.github.com>

---
+++ backend/src/main/java/com/uca/security/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.uca.security;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.uca.model.User;
+import com.uca.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                new ArrayList<>());
+    }
+}
